a29e2ff1da13b01548233365bad5a91c8d7c3c69
koshkakateyka <koneika336@gmail.com>
2025-04-02 10:28:28 -0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,37 @@ public class Main {
     public void fromCharToString(){
 
     }
+    public static boolean compareStrings(Scanner scanner, String forChar, String forString){
+//        System.out.println(forChar);
+//        System.out.println(forString);
+        scanner.nextLine();
+        if(Objects.equals(forChar, forString))
+            return true;
+        else
+            return false;
+
+
+//        for(int i = 0; i < forChar.length; i++){
+//            char c = forChar[i];
+//            System.out.print(c);
+//        }
+//        System.out.println();
+        // if compare is correct - true, if not - false
+//        return true;
+    }
     public static void exit(Scanner scanner){
         System.out.println("Exiting the program...");
         scanner.close();
         System.exit(0);
     }
     public static void saveas(String line, Scanner scanner){
-        switch (line){
-            default: System.out.println("Syntax error"); break;
-        }
+        // line should be divided, before and after the space
+        // if there isn't 2 words = skip
+        // first word is "saveas"
+        // the second word is path
+//        char[] lineToCharArray = line.toCharArray();
+//
+//        compareStrings(lineToCharArray, "word");
 //        if(path.isEmpty()) System.out.println("saveas <file>");
 //        char[] charArray1 = path.toCharArray();
 //        char[] charArray2 = { '.', 'x', 'm', 'l'};
@@ -23,10 +45,8 @@ public class Main {
 //                        System.out.println("Successfully saved another " + path + "\n.xml is exist");
 //                else System.out.println("empty name: " + Arrays.toString(charArray1) + "\nuse something like that: fileName" + Arrays.toString(charArray1));
 //            } catch (Exception e) { System.err.println(e.getMessage()); }
-//        scanner.nextLine();
     }
     public static void help(Scanner scanner){
-        String moreHelp = scanner.nextLine();
         System.out.println("""
                 The following commands are supported:
                 open <file>    opens <file>
@@ -36,20 +56,23 @@ public class Main {
                 help           prints this information
                 exit           exists the program
                 """);
-
-
     }
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print("> ");
+        // double check for Next and NextLine, it's for saveas especially
+
+
         while (scanner.hasNext()) {
-            String line = scanner.nextLine();
-            System.out.println(line);
+            String line = scanner.next(); // String line = scanner.nextLine();
+            String[] arrayCommands = {"open", "close", "save", "saveas", "help", "exit", "help open", "help close", "help save", "help saveas", "help help", "help exit"};
+            System.out.println(compareStrings(scanner, line, "saveas"));
+//            System.out.println(line);
             switch (line){
                 case "open": break;
                 case "close": break;
                 case "save": break;
-                case "saveas": break;
+                case "saveas": saveas(line, scanner); break;
                 case "help": break;
                 case "exit": break;
                 case "help open": break;
