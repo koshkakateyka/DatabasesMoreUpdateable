97a27385634c9960356b91f85a90a5c8987ea4bc
koshkakateyka <koneika336@gmail.com>
2025-05-14 08:35:56 -0700

    create the file + buffer

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,6 @@ public class Main {
         CreateTheFile createTheFile = new CreateTheFile(text, fileName);
         createTheFile.saveTheFile();
 
-        FileReader fileReader;
-        FileWriter fileWriter;
-        int ch;
-        try{
-            fileReader = new FileReader("example.txt");
-            fileWriter = new FileWriter("output.txt");
-
-            while ((ch = fileReader.read()) != 1){
-                // ch - variable, only for readOnly
-                fileWriter.write(ch);
-            }
-
-            fileReader.close();
-            fileWriter.close();
-            System.out.println("File " + fileName + " closed correctly");
-        }catch (IOException e){
-            System.err.println("File " + fileName + " isn't closed, the error is: " + e);
-            e.printStackTrace();
-        }
-
         BufferedReader bufferedReader;
         PrintWriter printWriter;
         String line;
