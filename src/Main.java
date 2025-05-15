f115dfb90c5739acda7b79171c69c8e6b22e15d5
koshkakateyka <koneika336@gmail.com>
2025-05-15 03:16:47 -0700

    tokenizer, but broken, check previous or wait one more commit

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,32 @@ public class Main {
         // write the full sentence, for out command line
         Scanner scanner = new Scanner(System.in);
 
-        functione(scanner.nextLine());
+        for (int i = 0; i < count(scanner.nextLine()); i++)
+
+//        functione(scanner.nextLine());
     }
 
     // https://stackoverflow.com/questions/7899525/how-to-split-a-string-by-space
-    public static void functione(String str){
+    public static int count(String str){
         // counting an every single word
         // actually, needs only for counting
         StringTokenizer tokens = new StringTokenizer(str, " ");
+
+        return tokens.countTokens();
+    }
+
+    public static void trimPLUSsplit(String str){
+
 
         // trim + split = delete no needed spaces
         // and divide our whole sentence to the every single word
         String[] splitStr = str.trim().split("\\s+");
 
-        // now, we have a good and checked words for put our words - to the command line
-        for(int i = 0; i < tokens.countTokens(); i++)
-            System.out.println(splitStr[i]);
+        return splitStr;
+
+//        // now, we have a good and checked words for put our words - to the command line
+//        for(int i = 0; i < tokens.countTokens(); i++)
+//            System.out.println(splitStr[i]);
     }
 
 }
