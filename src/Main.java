c412ab1a9857c32d8b7e26116baf7791179347c0
koshkakateyka <koneika336@gmail.com>
2025-05-17 11:04:01 -0700

    change general class Main to MyCmd

--- src/Main.java
+++ src/Main.java
@@ -7,102 +7,16 @@ public class Main {
         boolean open = false;
         String filePath = "";
 
+        MyCmd myCmd = new MyCmd(true, filePath);
+
         // main loop of cmd work
         while (true) {
-            String line = keyboardReader.getKeyboardInput();
-            AdvancedLine aLine = new AdvancedLine(line);
-
-            // after all, i wanna add Class MyCmd here
-            // open logic
-            if(aLine.index(0).equals("open")){
-                open = true;
-                // for concatinate
-                // fo example:
-                // input = open "C:\Temp\another
-                // but we lost file.xml"
-                // so, here is why i use ArrayList
-                // https://www.programiz.com/java-programming/arraylist
-                ArrayList<String> arrayList = new ArrayList<>();
-
-                for(int i = 1; i < aLine.countOfWords(); i++)
-                    arrayList.add(aLine.index(i));
-
-                // https://docs.vultr.com/java/standard-library/java/lang/String/join
-                // never use one quote like that: open 'filePath'
-                filePath = String.join(" ", arrayList).replaceAll("\"","");
-
-                System.out.println(filePath);
-
-                // https://programmingfundamental.github.io/courses/docs/object-oriented-programming-1-part/laboratory-exercise-10
-                OpenFile openFile = new OpenFile(filePath);
-                /*String data =*/ openFile.output();
-
-//                System.out.println(data);
-
-
-            }
-            // close logic
-            else if (open == true) {
-                if (aLine.index(0).equals("close")) {
-                    // save the data and close(like, you can't use again opened file, because you are close the file)
-                    // some logic here about close,
-                    System.out.println("Successfully closed " + filePath);
-                }
-            }
-            else if (open == false){
-                System.out.println("You can't close any file, because you never open anything, use it first:" + "\n" +
-                        "open <file>" + "\n" +
-                        "then: " + "\n" +
-                        "close" + "\n");
-            }
-            // save logic
-            else if (aLine.index(0).equals("save")) {
-
-            }
-            // saveas logic
-            else if (aLine.index(0).equals("saveas")) {
-                // for concatinate
-                // fo example:
-                // input = open "C:\Temp\another
-                // but we lost file.xml"
-                // so, here is why i use ArrayList
-                // https://www.programiz.com/java-programming/arraylist
-                ArrayList<String> arrayList = new ArrayList<>();
-
-                for(int i = 1; i < aLine.countOfWords(); i++)
-                    arrayList.add(aLine.index(i));
-
-                // https://docs.vultr.com/java/standard-library/java/lang/String/join
-                // never use one quote like that: open 'filePath'
-                filePath = String.join(" ", arrayList).replaceAll("\"","");
-
-                System.out.println(filePath);
-
-                // https://programmingfundamental.github.io/courses/docs/object-oriented-programming-1-part/laboratory-exercise-10
-                OpenFile openFile = new OpenFile(filePath);
-                /*String data =*/ openFile.output();
-
-//                System.out.println(data);
-
-
-            }
-            // help logic
-            else if (aLine.index(0).equals("help")) {
-                System.out.println("The following commands are supported:\n" +
-                        "open <file> opens <file>\n" +
-                        "close closes currently opened file\n" +
-                        "save saves the currently open file\n" +
-                        "saveas <file> saves the currently open file in <file>\n" +
-                        "help prints this information\n" +
-                        "exit exists the program");
-            }
-            // exit logic
-            else if (aLine.index(0).equals("exit")) {
-                System.out.println("Exiting the program...");
-                System.exit(0);
-            }
-
-
+            myCmd.open();
+            myCmd.close();
+            myCmd.save();
+            myCmd.saveas();
+            myCmd.help();
+            myCmd.exit();
         }
     }
 }
