d4a5bc0964f3eefda3887a7a353aa1af91cb6bb2
koshkakateyka <koneika336@gmail.com>
2025-05-20 11:17:30 +0300

    Delete Command.java

--- Command.java
+++
@@ -1,6 +0,0 @@
-// https://stackoverflow.com/questions/43872241/using-the-command-pattern-with-parameters?utm_source=chatgpt.com
-// https://www.baeldung.com/java-command-pattern
-
-public interface Command {
-    void execute();
-}
