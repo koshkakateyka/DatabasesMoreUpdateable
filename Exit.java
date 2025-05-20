5115050ea9682b81f22f40d1bb7caf8e1ae005ab
koshkakateyka <koneika336@gmail.com>
2025-05-20 11:17:38 +0300

    Delete Exit.java

--- Exit.java
+++
@@ -1,9 +0,0 @@
-// https://stackoverflow.com/questions/43872241/using-the-command-pattern-with-parameters?utm_source=chatgpt.com
-// https://www.baeldung.com/java-command-pattern
-public class Exit implements Command{
-
-    @Override
-    public void execute(){
-        System.exit(0);
-    }
-}
